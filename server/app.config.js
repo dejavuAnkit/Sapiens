7d733a1208644e845d3ca1a68408dddd00276056
dejavuAnkit <responsead1985@gmail.com>
2021-01-25 05:12:19 +0530

    Update app.config.js

--- server/app.config.js
+++ server/app.config.js
@@ -1,5 +1,5 @@
 const PORT = 3002;
-const DB_URL = "mongodb+srv://ankit:ankit1@cluster0.o5i4e.mongodb.net/sapiens?retryWrites=true&w=majority";
+const DB_URL = "mongo_url"
 const SECRET_KEY='VANDANBEEK';
 
 
